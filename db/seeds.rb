ce769609749421d70435d8ec15b23d47807d39f3
Gabriel Majeri <gabriel.majeri6@gmail.com>
2024-03-30 23:31:51 +0200

    Refactor and expand database seed code

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,63 +9,82 @@ admin_entra_user_id = '14ad39ca-6e68-412c-9b68-f9b71f4478d4'
 
 admin = User.find_or_initialize_by(entra_user_id: admin_entra_user_id)
 
-identity_platform_credentials = Rails.application.credentials.microsoft_identity_platform
+unless admin.persisted?
+  identity_platform_credentials = Rails.application.credentials.microsoft_identity_platform
 
-context = MicrosoftKiotaAuthenticationOAuth::ClientCredentialContext.new(
-  identity_platform_credentials[:tenant_id],
-  identity_platform_credentials[:client_id],
-  identity_platform_credentials[:client_secret],
-)
+  context = MicrosoftKiotaAuthenticationOAuth::ClientCredentialContext.new(
+    identity_platform_credentials[:tenant_id],
+    identity_platform_credentials[:client_id],
+    identity_platform_credentials[:client_secret],
+  )
 
-authentication_provider = MicrosoftGraphCore::Authentication::OAuthAuthenticationProvider.new(
-  context,
-  nil,
-  ['https://graph.microsoft.com/.default']
-)
+  authentication_provider = MicrosoftGraphCore::Authentication::OAuthAuthenticationProvider.new(
+    context,
+    nil,
+    ['https://graph.microsoft.com/.default']
+  )
 
-adapter = MicrosoftGraph::GraphRequestAdapter.new(authentication_provider)
-client = MicrosoftGraph::GraphServiceClient.new(adapter)
+  adapter = MicrosoftGraph::GraphRequestAdapter.new(authentication_provider)
+  client = MicrosoftGraph::GraphServiceClient.new(adapter)
 
-result = client.users.by_user_id(admin_entra_user_id).get.resume
+  result = client.users.by_user_id(admin_entra_user_id).get.resume
 
-admin.entra_user_id = result.id
-admin.email = result.mail
-admin.first_name = result.given_name
-admin.last_name = result.surname
-admin.role = :admin
+  admin.entra_user_id = result.id
+  admin.email = result.mail
+  admin.first_name = result.given_name
+  admin.last_name = result.surname
+  admin.role = :admin
 
-admin.save!
+  admin.save!
+end
 
 current_year_setting = Setting.find_or_initialize_by(key: :current_year)
-current_year_setting.value = Time.zone.today.year
-current_year_setting.save!
-
-[
-  'Buget', 'Finanțare complementară', 'Cercetare',
-  'Spații de învățământ', 'Cămine', 'Erasmus',
-  'CIVIS', 'FDI', 'Venituri',
-].each do |financing_source_name|
-  FinancingSource.find_or_create_by!(name: financing_source_name)
+unless current_year_setting.persisted?
+  current_year_setting.value = Time.zone.today.year
+  current_year_setting.save!
 end
 
-[
-  'Proiect intern UB', 'Național', 'Proiect cu TVA',
-  'Internațional', 'PNRR', 'SEE',
-].each do |project_category_name|
-  ProjectCategory.find_or_create_by!(name: project_category_name)
+seed_files_directory = Rails.root / 'db' / 'seeds'
+
+csv_reader_options = {
+  headers: true,
+  converters: -> (f) { f&.strip }
+}
+
+puts 'Seeding financing sources...'
+
+CSV.foreach(seed_files_directory / 'financing_sources.csv', **csv_reader_options) do |row|
+  name = row[0]
+  puts "Creating financing source '#{name}'"
+  FinancingSource.find_or_create_by!(name:)
 end
 
-{
-  '10.01.01': 'Salarii de bază',
-  '10.01.02': 'Salarii de merit',
-  '10.01.03': 'Indemnizație de conducere',
-  '10.01.04': 'Spor de vechime',
-}.each do |code, name|
+puts 'Seeding project categories...'
+
+CSV.foreach(seed_files_directory / 'project_categories.csv', **csv_reader_options) do |row|
+  name = row[0]
+  puts "Creating project category '#{name}'"
+  ProjectCategory.find_or_create_by!(name:)
+end
+
+puts 'Seeding expenditure articles...'
+
+CSV.foreach(seed_files_directory / 'expenditure_articles.csv', **csv_reader_options) do |row|
+  code = row[0]
+  name = row[1]
+  expenditure_category_code = row[2]
+  commitment_category_code = row[3]
+  puts "Creating expenditure article with code '#{code}', name '#{name}'"
   expenditure_article = ExpenditureArticle.find_or_initialize_by(code:)
   expenditure_article.name = name
+  expenditure_article.expenditure_category_code = expenditure_category_code
+  expenditure_article.commitment_category_code = commitment_category_code
   expenditure_article.save!
 end
 
-['Avans numerar', 'Avans virament', 'Numerar', 'Virament'].each do |payment_method_name|
-  PaymentMethod.find_or_create_by!(name: payment_method_name)
+puts 'Seeding payment methods...'
+
+['Avans numerar', 'Avans virament', 'Numerar', 'Virament'].each do |name|
+  puts "Creating project category '#{name}'"
+  PaymentMethod.find_or_create_by!(name:)
 end
