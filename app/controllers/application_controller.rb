1619a0b12add010efc2d1b5836c2bc6da798f42a
Gabriel Majeri <gabriel.majeri6@gmail.com>
2024-12-02 12:17:48 +0200

    Implicitly filter expenditures/commitments by current year

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,16 @@ class ApplicationController < ActionController::Base
   before_action :require_login
 
   helper_method :current_user
+  helper_method :current_year
 
   protected
 
   def current_user
     @current_user ||= User.find(session[:current_user_id])
+  end
+
+  def current_year
+    @current_year ||= Setting.current_year
   end
 
   private
