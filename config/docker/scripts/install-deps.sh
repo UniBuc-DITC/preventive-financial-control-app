0eeae28fc2c9b4de1e6a24ee7e1ffb506df4a8f5
Gabriel Majeri <gabriel.majeri6@gmail.com>
2026-01-11 19:24:46 +0200

    Fix dependencies install script

--- config/docker/scripts/install-deps.sh
+++ config/docker/scripts/install-deps.sh
@@ -7,7 +7,7 @@ set -ex
 
 # Install libpq, required by PostgreSQL gem
 apt-get update
-apt-get install libpq-dev
+apt-get install -y libpq-dev
 
 # Install Node.js (required for preprocessing assets)
 apt-get update
@@ -16,7 +16,7 @@ apt-get install -y ca-certificates curl gnupg
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 
-NODE_MAJOR=22
+NODE_MAJOR=24
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
 
 apt-get update
